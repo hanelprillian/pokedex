e88f81c3582f4464ba1ff19c6353317c8045e9ad
Hanel Kororo <hanel@kororo.co>
2021-11-28 00:56:51 +0700

    remove comment

--- src/main.js
+++ src/main.js
@@ -18,9 +18,8 @@ const routes = [
 ]
 
 const router = createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: createWebHistory(),
-    routes, // short for `routes: routes`
+    routes
 })
 
 const app = createApp(App)
